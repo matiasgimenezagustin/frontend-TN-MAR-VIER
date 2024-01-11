e0b7d343f3efc8047ad91da357bda9ab3985ce65
matu.dev <matu.a.gimenez2002@gmail.com>
2024-01-11 18:47:03 -0300

    act

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const mode = "PROD"
 
 const URL_API = 
 mode == 'PROD' 
-? 'https://backend-tn-mar-vier-production.up.railway.app' 
+? 'https://test-1-b666.onrender.com/' 
 : 'http://localhost:8080'
 
 export {URL_API}
