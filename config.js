533149f4366386b4425c41c73c679e0802884066
matu.dev <matu.a.gimenez2002@gmail.com>
2024-01-11 20:47:10 -0300

    act

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const mode = "PROD"
 
 const URL_API = 
 mode == 'PROD' 
-? 'https://test-1-b666.onrender.com' 
+? 'https://backend-tn-mar-jue.onrender.com' 
 : 'http://localhost:8080'
 
 export {URL_API}
